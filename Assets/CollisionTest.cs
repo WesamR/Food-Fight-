678da887b17f2794a8cad039dd949175c538627a
WesamR <113917519+WesamR@users.noreply.github.com>
2024-10-23 15:15:04 -0400

    CollisionAdd
    Checked if collision works. it destroyes test fruitOBJ when in contact. spawner still no work

---
+++ Assets/CollisionTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject); // Destroys the fruit clone the player touches.
+        }
+    }
+}
