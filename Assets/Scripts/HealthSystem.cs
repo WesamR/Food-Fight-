17a9a3cf51726a02a1564f6893356b7babea15da
pingu543 <pcu50208@outlook.com>
2024-10-23 20:50:36 -0400

    Improved HealthSystem
    Basic appearance but ready for testing food damage. Debug button changed from changing dead status to Eat().

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,31 +1,77 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class HealthSystem : MonoBehaviour
 {
+    public int currentCalories;
+    public int maxCalories = 10000;
     private bool isDead = false;
+
+    private GameObject caloriesDisplayObject;
+    private TextMesh caloriesTextMesh;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Create a new GameObject for the TextMesh
+        caloriesDisplayObject = new GameObject("CaloriesDisplay");
+        caloriesTextMesh = caloriesDisplayObject.AddComponent<TextMesh>();
+
+        // Configure the TextMesh
+        caloriesTextMesh.text = currentCalories + "/" + maxCalories;
+        caloriesTextMesh.characterSize = 0.1f;
+        caloriesTextMesh.fontSize = 32;
+        caloriesTextMesh.color = Color.white;
+        caloriesTextMesh.anchor = TextAnchor.MiddleCenter;  // Center the text
+
+        // Set the TextMesh to be a child of the player
+        caloriesDisplayObject.transform.SetParent(transform);
+
+        // Position the TextMesh above the player character
+        caloriesDisplayObject.transform.localPosition = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Ensure the caloriesDisplayObject stays above the player
+        caloriesDisplayObject.transform.localPosition = new Vector3(0, 1, 0);
+        caloriesTextMesh.text = currentCalories + "/" + maxCalories;
+
     }
 
+    /// <summary>
+    /// This method is called when the player eats food.
+    /// If the player's calories exceed the maximum, the player is dead.
+    /// </summary>
+    /// <param name="calories">The calories value of the food.</param>
+    public void Eat(int calories)
+    {
+        currentCalories += calories;
+        if (currentCalories >= maxCalories)
+        {
+            currentCalories = maxCalories;
+            isDead = true;
+        }
+    }
+
+    /// <summary>
+    /// Returns the player's current death status.
+    /// </summary>
     public bool IsDead
     {
         get { return isDead; }
     }
 
+    /// <summary>
+    /// Debug method to unalive the player.
+    /// </summary>
+    /// <param name="context"></param>
     public void Unalive(InputAction.CallbackContext context)
     {
-        isDead = true;
+        Eat(10);
+        //isDead = true;
         //Debug.Log("Player is dead");
     }
 
